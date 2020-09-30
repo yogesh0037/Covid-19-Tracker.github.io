c291d4a13042a0312f98acfad4e480e0b854b6b7
yogesh <yogeshsharma0037@gmail.com>
2020-09-30 18:51:12 +0530

    Updates

---
+++ precache-manifest.8db20d2e841acd1715a38143d049604f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87fbc6fd895c617528ca068f5748482",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "40f375a051ae1c87405a",
+    "url": "/Covid-19-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "e375a66d46bed7a84336",
+    "url": "/Covid-19-Tracker/static/css/main.2cf71a8e.chunk.css"
+  },
+  {
+    "revision": "40f375a051ae1c87405a",
+    "url": "/Covid-19-Tracker/static/js/2.d9006698.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-19-Tracker/static/js/2.d9006698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e375a66d46bed7a84336",
+    "url": "/Covid-19-Tracker/static/js/main.2a1e7102.chunk.js"
+  },
+  {
+    "revision": "a7c7f2523555197cd99e",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.8c5aad39.js"
+  }
+]);
